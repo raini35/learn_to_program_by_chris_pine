31b08a40d91ae4552f10352348b45127ad61f3fa
raini35 <rainier.f.go@gmail.com>
2016-08-06 00:02:24 -0400

    Finished chapter 2

---
+++ 2_letters/print.rb
@@ -0,0 +1,25 @@
+puts 'Hello world!' 
+puts ''
+puts 'Good-bye.'
+
+puts 'String arithmetic:' 
+puts 'I like' + 'apple pie.'
+puts 'I like ' + 'apple pie.' 
+puts 'I like' + ' apple pie.'
+puts ''
+puts 'Multiplying a number with a string: ' 
+puts 'blink ' * 4
+puts '' 
+puts 'Number vs string: ' 
+puts 12 + 12 
+puts '12' + '12'
+puts '12 + 12'
+puts 2 * 5 
+puts '2' * 5 
+puts '2 * 5'
+puts ''
+puts 'Escape sequences: '
+puts 'You\'re swell!'
+puts 'backslash at the end of string: \\'
+puts 'up\\down'
+puts 'up\down'
